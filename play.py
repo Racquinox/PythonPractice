dd653b6856f974ea1a45d45d66475467329249e5
Racquinox <racquinox@gmail.com>
2025-06-03 23:48:24 -0500

    Made a number guessing game

--- play.py
+++ play.py
@@ -1,1 +1,20 @@
-print("Hellow World");
+import random
+
+num = random.randint(1, 10)
+
+print("Guess a number from 1 to 10")
+guess = int(input())
+
+
+while guess != num:
+
+    if guess > num:
+        print("Too high, try again")
+        guess = int(input())
+
+    elif guess < num:
+        print("Too low, try again")
+        guess = int(input())
+
+else:
+    print("You got it!")
